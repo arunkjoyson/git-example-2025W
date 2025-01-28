5a62c3d5f93c4de05638bf3c7d1165e354f21ee4
arun777akj <arun777akj@gmail.com>
2025-01-28 15:46:46 -0500

    removed the unrequired files

--- sentence_lowercase_function.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-function to_lowercase() {
-    echo "$1" | tr '[:upper:]' '[:lower:]'
-}
--- sentence_lowercase_main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# execute the function file
-source ./sentence_lowercase_function.sh
-
-# Input data
-sentence="HeLlLo"
-lowercase_sentence=$(to_lowercase "$sentence")
-echo "$lowercase_sentence
