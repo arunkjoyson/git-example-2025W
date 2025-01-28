e6662c0dbf881834d03a357a9eb423ceb80af2bb
arun777akj <arun777akj@gmail.com>
2025-01-28 15:09:01 -0500

    added file for sum of 4 numbers error function:

---
+++ sum_of_four_numbers_function.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$#" -eq 4 ]; then
+    echo "Error: 4 arguments can't be taken."
+else
+    echo "Arguments are within the limit."
+fi
---
+++ sumof4numbers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# sum of 4 arguments shows error
+
+
+RESULT=$(./sum_of_four_numbers_function.sh 4 5 3 8)
+
+echo "$RESULT"
+
